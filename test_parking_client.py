1155793fd5167d82185ffd156cc199b6822ed00c
YojanP <yojans.palaciosg@uqvirtual.edu.co>
2025-05-29 02:29:35 -0500

    Update test_parking_client.py

--- test_parking_client.py
+++ test_parking_client.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from new_window import RegistroVentana  # la ventana que definiste
+from interfaz_local import RegistroVentana  # la ventana que definiste
 
 
 url = "http://localhost:80"
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     ventana = RegistroVentana(url)  # Pasa la URL para usar en la clase
     ventana.show()
     sys.exit(app.exec_())
+
 
 
